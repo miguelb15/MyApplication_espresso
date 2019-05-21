4ad3f64e09724bd811f6fdc2fbce90ca6b18641d
AlexGuti14 <a.guti3@hotmail.com>
2019-05-21 10:41:25 +0200

    Test

---
+++ app/src/androidTest/java/com/example/miguel/myapplication_espresso/MainActivityEspressoTest.java
@@ -0,0 +1,43 @@
+package com.example.miguel.myapplication_espresso;
+
+import static org.junit.Assert.*;
+import androidx.test.rule.ActivityTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+public class MainActivityEspressoTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void ensureTextChangesWork() {
+        // Type text and then press the button.
+        onView(withId(R.id.inputField))
+                .perform(typeText("HELLO"), closeSoftKeyboard());
+        onView(withText("Change Text")).perform(click());
+        // Check that the text was changed.
+        onView(withId(R.id.inputField)).check(matches(withText("Lalala")));
+    }
+
+
+    @Test
+    public void ensureLaunchSecondActivityWithInputTextWork() {
+        // Type text and then press the button.
+        onView(withId(R.id.inputField)).perform(typeText("NewText"),
+                closeSoftKeyboard());
+        onView(withText("Switch Activity")).perform(click());
+        // This view is in a different Activity, no need to tell Espresso.
+        onView(withId(R.id.resultView)).check(matches(withText("NewText")));
+    }
+
+
+}
